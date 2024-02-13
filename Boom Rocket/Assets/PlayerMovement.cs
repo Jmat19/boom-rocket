1c4f972f67e73bef61d75b1299da98ef77d937c8
Jmat19 <47995875+Jmat19@users.noreply.github.com>
2024-02-13 16:45:07 -0500

    Added unfinished code

--- Boom Rocket/Assets/PlayerMovement.cs
+++ Boom Rocket/Assets/PlayerMovement.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private float maxSpeed;
+
+    InputSystem inputActions;
+    InputAction move;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        inputActions
     }
 
     // Update is called once per frame
