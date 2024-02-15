536ae15b937d711e377c8b4809bea0efa380347f
Jmat19 <47995875+Jmat19@users.noreply.github.com>
2024-02-14 20:56:29 -0500

    Player Controller Complete

--- Boom Rocket/Assets/PlayerMovement.cs
+++ Boom Rocket/Assets/PlayerMovement.cs
@@ -11,21 +11,70 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float maxSpeed;
 
+    [SerializeField]
+    private float jumpSpeed;
+
+    [SerializeField]
+    private bool isGrounded;
+
+    private Vector3 forceDirection;
+
     InputSystem inputActions;
     InputAction move;
 
     private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        inputActions
+        inputActions = new InputSystem();
+        isGrounded = true;
+    }
+
+    void OnEnable()
+    {
+        inputActions.Land.Jump.performed += OnJump;
+        move = inputActions.Land.Movement;
+        inputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Land.Jump.performed -= OnJump;
+        inputActions.Disable();
+    }
+
+    void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        forceDirection += move.ReadValue<Vector2>().x * transform.right * movementSpeed;
+        forceDirection += move.ReadValue<Vector2>().y * transform.forward * movementSpeed;
+
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        forceDirection = Vector3.zero;
+
+        Vector3 horizontalVel = rb.velocity;
+        horizontalVel.y = 0;
+        if (horizontalVel.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            rb.velocity = horizontalVel.normalized * maxSpeed + Vector3.up * rb.velocity.y;
+        }
     }
 }
